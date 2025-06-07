fcaf075b75c229be13d82fed307f4e24af753dbe
binhcoder08 <139665502+binhcoder08@users.noreply.github.com>
2025-06-07 16:58:53 +0700

    updategg

--- src/components/Login.js
+++ src/components/Login.js
@@ -23,7 +23,7 @@ const Login = () => {
 
   const validatePassword = (value) => {
     if (!value) return "Mật khẩu không được để trống";
-    if (value.length < 8) return "Mật khẩu phải từ 8 ký tự trở lên";
+    if (value.length < 5) return "Mật khẩu phải từ 8 ký tự trở lên";
     return null;
   };
 
