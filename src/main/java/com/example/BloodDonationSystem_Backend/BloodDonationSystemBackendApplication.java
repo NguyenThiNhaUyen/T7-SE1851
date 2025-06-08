c57a05135bd6d4b66932f18c6a78fd49f1660475
Tran Tan Phat <Trantanphat2004@gmail.com>
2025-06-08 07:52:47 +0700

    Tao du an moi

---
+++ src/main/java/com/example/BloodDonationSystem_Backend/BloodDonationSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.BloodDonationSystem_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodDonationSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodDonationSystemBackendApplication.class, args);
+	}
+
+}
