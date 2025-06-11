a0868354fbfa50f04eec017feda53e3d60925f88
TranVanQuyet04 <tranquyetcoder@gmail.com>
2025-06-11 18:03:25 +0700

    admin update

--- src/main/java/com/quyet/superapp/entity/User.java
+++ src/main/java/com/quyet/superapp/entity/User.java
@@ -13,59 +13,26 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "UserID")
-    private Integer id;
+    @Column(name = "User_Id")
+    private Long user_id;
 
-    @Column(name = "Username", nullable = false, unique = true, columnDefinition = "VARCHAR")
+    @Column(name = "UserName", columnDefinition = "NVARCHAR(100)", nullable = false)
     private String username;
 
-    @Column(name = "Email", nullable = false, unique = true, columnDefinition = "VARCHAR")
+    @Column(name = "Password", columnDefinition = "VARCHAR(255)", nullable = false)
+    private String password; // đủ dài cho BCrypt nếu sau này dùng
+
+    @Column(name = "IsEnable")
+    private boolean isEnable;
+
+    @ManyToOne(fetch = FetchType.LAZY) // mặc định sẽ load role luôn khi truy vấn user
+    @JoinColumn(name = "Role_Id") //Khóa ngoại của bảng Users
+    private Role role;
+
+
+    @Column(name = "Email", columnDefinition = "VARCHAR(50)", nullable = false, unique = true)
     private String email;
 
-    @Column(name = "Password", columnDefinition = "VARCHAR")
-    private String password;
-
-    @Column(name = "Role", columnDefinition = "VARCHAR")
-    private String role; // Guest, Member, Staff, Admin
-
-    @Column(name = "Enable")
-    private Boolean enable;
-
-    // Quan hệ 1-1
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
-    private UserProfile userProfile;
-
-    // Quan hệ 1-n
-    @OneToMany(mappedBy = "user")
-    private List<Donation> donations;
-
-    @OneToMany(mappedBy = "user")
-    private List<DonationRegistration> donationRegistrations;
-
-    @OneToMany(mappedBy = "requester")
-    private List<BloodRequest> bloodRequests;
-
-    @OneToMany(mappedBy = "user")
-    private List<Reminder> reminders;
-
-    @OneToMany(mappedBy = "user")
-    private List<ChatLog> chatLogs;
-
-    @OneToMany(mappedBy = "user")
-    private List<ScanLog> scanLogs;
-
-    @OneToMany(mappedBy = "scannedBy")
-    private List<ScanLog> scannedLogs;
-
-    @OneToMany(mappedBy = "author")
-    private List<Blog> blogs;
-
-    @OneToMany(mappedBy = "user")
-    private List<ContactForm> contactForms;
-
-    @OneToMany(mappedBy = "generatedBy")
-    private List<Report> reports;
-
-    @OneToMany(mappedBy = "user")
-    private List<Notification> notifications;
+    private UserDetail userDetail;
 }
